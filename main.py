e4ab56b92b0ad66c435154002ac833bd70840109
Глеб Андров <anderman309@gmail.com>
2025-02-28 11:44:41 +0300

    added materials

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, session, request, redirect, url_for, f
 
 app = Flask(__name__)
 
+@app.route('/')
+def statistics_by_region():
+    return render_template('stat.html')
+
+@app.route('/stat_by_school')
+def statistics_by_school():
+    return render_template('stat.html')
+
+@app.route('/results')
+def results():
+    return render_template('stat.html')
+
+@app.route('/dashboard/<int:id>')
+def dashboard(id):
+    return render_template('stat.html')
 
 
 if __name__ == "__main__":
