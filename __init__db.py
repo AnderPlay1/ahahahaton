91474b6f7584c996eda1f0ddd9986d370b332afc
nik_or_trick <nikolas.kravtsov@gmail.com>
2025-02-28 13:07:10 +0300

    add db_functions.py, configurating db in __init__db.py

---
+++ __init__db.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+connect = sqlite3.connect("Database.bd")
+cursor = connect.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Scores (
+        ID      INTEGER PRIMARY KEY AUTOINCREMENT,
+        ID_user INTEGER REFERENCES Users (ID),
+        time    TEXT,
+        task_1   INTEGER,
+        task_2   INTEGER,
+        task_3   INTEGER,
+        task_4   INTEGER,
+        task_5   INTEGER,
+        task_6   INTEGER,
+        task_7   INTEGER,
+        task_8   INTEGER,   
+        rank TEXT
+    );
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Users (
+        ID      INTEGER PRIMARY KEY AUTOINCREMENT,
+        name    TEXT,
+        surname TEXT,
+        patronymic TEXT,
+        form INTEGER,
+        region  TEXT,
+        school  TEXT
+    );
+""")
+connect.commit()
+connect.close()
---
+++ db_functions.py
@@ -0,0 +1,50 @@
+import sqlite3
+
+connect = sqlite3.connect("Database.db")
+cursor = connect.cursor()
+
+def add_user(data):
+    """
+    :param data: Dict{name:str, surname:str, patronymic:str, form:int, region:str, school:str}
+    :return: -
+    """
+    params = [data['name'], data['surname'], data['patronymic'], data['region'], data['school']]
+    cursor.execute("INSERT INTO Users (name, surname, patronymic, form, region, school) VALUES (?, ?, ?, ?, ?, ?)", params)
+    connect.commit()
+
+def add_curent_results(data, time):
+    """
+    :param data: List[Dict{name:str, surname:str, patronymic:str, form:int, region:str, school:str, rank:str, score:List[int]}]
+    :param time: str
+    :return: -
+    """
+    for item in data:
+        params = [data['name'], data['surname'], data['patronymic'], data["form"], data['region'], data['school']]
+        id_user = cursor.execute("SELECT ID FROM Users WHERE name = ? AND surname = ? AND patronymic = ? AND form = ? AND region = ? AND school = ?", params).fetchall()
+        if len(id_user) == 0:
+            add_user(item)
+            id_user = cursor.execute(
+                "SELECT ID FROM Users WHERE name = ? AND surname = ? AND patronymic = ? AND form = ? AND region = ? AND school = ?", params).fetchall()
+        id_user = id_user[0][0]
+        params = [id_user, time, item["rank"]] + item["score"]
+        cursor.execute("INSERT INTO Scores (ID_user, time, rank, task_1, task_2, task_3, task_4, task_5, task_6, task_7, task_8) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", params)
+    connect.commit()
+
+def get_final_sum_for_user(id_user):
+    """
+    :param id_user: int
+    :return: result:int
+    """
+    result = cursor.execute("SELECT sum(task_1, task_2, task_3, task_4, task_5, task_6, task_7, task_8) FROM Scores WHERE ID_user = ? ORDER BY time LIMIT 1").fetchall()
+    if len(result) == 0:
+        result = 0
+    else:
+        result = sum(result[0])
+    return result
+
+def get_all_students():
+    """
+    :return: List[Tuple(ID:int, name:str, surname:str, patronymic:str, form:int, region:str, school:str)]
+    """
+    result = cursor.execute("SELECT * FROM Users").fetchall()
+    return result
